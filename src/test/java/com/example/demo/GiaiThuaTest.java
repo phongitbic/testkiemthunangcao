c8204daf3bca4def9f2328879912a4dbf3edac86
TranTrungPhong <achinpo1999@gmail.com>
2024-10-31 08:18:25 +0700

    thanhcong

---
+++ src/test/java/com/example/demo/GiaiThuaTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GiaiThuaTest {
+
+    @Test
+    void tinhGiaiThua() {
+
+    }
+}
