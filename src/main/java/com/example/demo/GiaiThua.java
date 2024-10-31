a4a4754b7b5a842b39bfb59f5d937e2cfd6a5156
TranTrungPhong <achinpo1999@gmail.com>
2024-10-31 08:18:00 +0700

    thanhcong

--- src/main/java/com/example/demo/GiaiThua.java
+++ src/main/java/com/example/demo/GiaiThua.java
@@ -2,11 +2,18 @@ package com.example.demo;
 
 public class GiaiThua {
 
-    public int tinhGiaiThua(int n){
+    public int tinhGiaiThua(int n) {
 
+        int ketQua = 1;
 
+            for (int i = 1; i < n; i++) {
+                ketQua *=i;
+            }
 
-
-        return n;
+        return ketQua;
+    }
+    public static void main (String[]args){
+        GiaiThua giaiThua = new GiaiThua();
+        System.out.println(giaiThua.tinhGiaiThua(6));
     }
 }
